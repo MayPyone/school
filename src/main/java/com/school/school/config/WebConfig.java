45d868d52a37c10b214b3eae29b84e0046eb9462
maypyone <maypyone015@gmail.com>
2026-05-18 22:33:16 +0630

    Update url

--- src/main/java/com/school/school/config/WebConfig.java
+++ src/main/java/com/school/school/config/WebConfig.java
@@ -22,7 +22,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/v1/**")
-                .allowedOrigins("http://localhost:5173", "https://school-o3fd.onrender.com/")
+                .allowedOrigins("http://localhost:5173", "https://school-o3fd.onrender.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);

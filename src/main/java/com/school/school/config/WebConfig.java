b2049ffd8779db69e4f0c3c77dee3b9e6fab9e01
maypyone <maypyone015@gmail.com>
2026-05-18 21:44:36 +0630

    Update cors policy

--- src/main/java/com/school/school/config/WebConfig.java
+++ src/main/java/com/school/school/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.school.school.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,5 +17,14 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authenticationInterceptor)
                 .addPathPatterns("/api/v1/**");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/v1/**")
+                .allowedOrigins("http://localhost:5173", "https://school-o3fd.onrender.com/")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
